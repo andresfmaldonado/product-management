6f3a3a2e29f394453b0add441e19d3bd0498d740
Andres Maldonado <desarrollo13@domina.com.co>
2024-05-17 16:32:19 -0500

    Fix column names migrations

--- database/migrations/2024_05_17_204054_create_products_table.php
+++ database/migrations/2024_05_17_204054_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre', 50);
-            $table->float('precio', 10, 2);
+            $table->string('name', 50);
+            $table->float('price', 10, 2);
             $table->integer('stock');
             $table->timestamps();
         });
