5fbf3dd53ec60cb76a9d78a306047812e592719f
Andres Maldonado <desarrollo13@domina.com.co>
2024-05-17 16:46:20 -0500

    Add Product Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(function() {
+    Route::prefix('product')->group(function() {
+        Route::post('', [ProductController::class, 'store']);
+        Route::get('', [ProductController::class, 'getAll']);
+        Route::get('/{id}', [ProductController::class, 'getById']);
+        Route::post('/category', [ProductController::class, 'getByCategory']);
+        Route::post('/price', [ProductController::class, 'getByPrice']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
+    })
+})
