c6b5db103707f519d4d69e0dc4a8ac8eb7387676
Andres Maldonado <desarrollo13@domina.com.co>
2024-05-17 15:57:55 -0500

    Add relations belongs To Many

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     use HasFactory;
+
+    /**
+     * The products that belong to the categories
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Products::class, 'product_categories', 'product_id', 'category_id');
+    }
 }
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     use HasFactory;
+
+    /**
+     * The categories that belong to the products
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Categories::class, 'product_categories', 'product_id', 'category_id');
+    }
 }
